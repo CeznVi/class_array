d1e8ee1ec99516d26e1ebd2ea108678d7295ae52
Viktor <Cezn.Vi@gmail.com>
2022-07-30 10:58:46 +0300

    fix

--- Array.h
+++ Array.h
@@ -11,18 +11,15 @@ class Array
 
 public:
 	Array() {}
-
 	explicit Array(int n)
 	{
 		size = n;
 		arr = new T[n] {0};
 	}
-
 	~Array()
 	{
 		delete[] arr;
 	}
-
 	Array(const Array& obj)
 	{
 		size = obj.size;
@@ -32,7 +29,6 @@ public:
 			arr[i] = obj.arr[i];
 		}
 	}
-
 	void set(int min = 0, int max = 9) const 
 	{
 		for (size_t i = 0; i < size; i++)
@@ -40,7 +36,6 @@ public:
 			arr[i] = rand() % (max - min + 1) + min;
 		}
 	}
-
 	void print() const
 	{
 		for (size_t i = 0; i < size; i++)
@@ -49,11 +44,12 @@ public:
 		}
 		cout << endl;
 	}
-
 	T& operator[](int ind) const;
 	void operator()(int incr) const;
-
+	
+	
 };
+
 
 template<class T>
 T& Array<T>::operator[](int ind) const
--- Source.cpp
+++ Source.cpp
@@ -5,18 +5,20 @@
 int main()
 {
 	//char
-	Array<char> arrChar(10);
+	const int size = 10;
+	Array<char> arrChar(size);
 	arrChar.set(50,60);
 	arrChar.print();
-	arrChar[10] = "s";
+	arrChar[0] = 30;
+	arrChar.print();
 	arrChar(5);
 	arrChar.print();
-
+	std::cout << "a = " << a << '\n';
 
 
 	 
 	//int
-	Array<int> arrInt(10);
+	Array<int> arrInt(size);
 	arrInt.set(0, 10);
 	arrInt.print();
 	arrInt[1] = 1400;
